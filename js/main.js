cdf4bc2e635cf70b4060ce01a39f9e07a7f034a7
cooleymt <cooleymt@mail.uc.edu>
2018-11-11 12:32:49 -0500

    Add enamies

--- js/main.js
+++ js/main.js
@@ -14,6 +14,51 @@ var game = new Phaser.Game(config);
  
 var graphics;
 var path;
+var ENEMY_SPEED = 1/10000;
+
+var Enemy = new Phaser.Class({
+ 
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+    function Enemy (scene)
+    {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'enemy');
+        this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
+    },
+    startOnPath: function ()
+    {
+        // set the t parameter at the start of the path
+        this.follower.t = 0;
+            
+        // get x and y of the given t point            
+        path.getPoint(this.follower.t, this.follower.vec);
+            
+        // set the x and y of our enemy to the received from the previous step
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+            
+    },
+    update: function (time, delta)
+    {
+        // move the t point along the path, 1 is the start and 0 is the end
+        this.follower.t += ENEMY_SPEED * delta;
+            
+        // get the new x and y coordinates in vec
+        path.getPoint(this.follower.t, this.follower.vec);
+        
+        // update enemy x and y to the newly obtained x and y
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+        // if we have reached the end of the path, remove the enemy
+        if (this.follower.t >= 1)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    }
+
+});
  
 function preload() {
     // load the game assets – enemy and turret atlas
@@ -22,9 +67,39 @@ function preload() {
 }
  
 function create() {
+    // this graphics element is only for visualization, 
+    // its not related to our path
+    graphics = this.add.graphics();    
     
+    // the path for our enemies
+    // parameters are the start x and y of our path
+    path = this.add.path(96, -32);
+    path.lineTo(96, 164);
+    path.lineTo(480, 164);
+    path.lineTo(480, 544);
+    
+    graphics.lineStyle(3, 0xffffff, 1);
+    // visualize the path
+    path.draw(graphics);
+
+    enemies = this.add.group({ classType: Enemy, runChildUpdate: true });
+	this.nextEnemy = 0;
 }
  
-function update() {
-    
+function update(time, delta) {
+    // if its time for the next enemy
+    if (time > this.nextEnemy)
+    {        
+        var enemy = enemies.get();
+        if (enemy)
+        {
+            enemy.setActive(true);
+            enemy.setVisible(true);
+            
+            // place the enemy at the start of the path
+            enemy.startOnPath();
+            
+            this.nextEnemy = time + 2000;
+        }       
+    }
 }
